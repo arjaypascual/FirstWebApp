44b25669e9d7f619260e731d1499eb005ea8f287
arjaypascual <ddoooxxxx11@gmail.com>
2025-11-26 12:17:03 +0000

    Update message

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
-const http = require("http");
+const express = require("express");
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.write("Hello Cloud!!!");
-  res.end();
+// allow JSON body
+app.use(express.json());
+
+// Home page
+app.get("/", (req, res) => {
+  res.send("Quotes API Online!");
 });
 
-server.listen(PORT, () => {
+// POST insert (name, section, quotation)
+app.post("/addQuote", (req, res) => {
+  const { name, section, quotation } = req.body;
+
+  if (!name || !section || !quotation) {
+    return res.status(400).json({
+      error: "name, section, and quotation are required."
+    });
+  }
+
+  res.json({
+    message: "Quote received!",
+    data: { name, section, quotation }
+  });
+});
+
+// Start server
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
